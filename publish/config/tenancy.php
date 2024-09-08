5cbc20b9accf7f6ca0738d928acdbec8bd6bb2e5
Fady Mondy <info@3x1.io>
2024-09-08 13:50:59 +0300

    fix CENTERAL_DOMAIN in config

--- publish/config/tenancy.php
+++ publish/config/tenancy.php
@@ -15,7 +15,7 @@ return [
      *
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
-    'central_domains' => array_filter(array_merge([env('APP_BASE_DOMAIN','')], explode(',',env('CENTRAL_DOMAINS','')))),
+    'central_domains' => array_filter(array_merge([env('CENTRAL_DOMAIN','')], explode(',',env('CENTRAL_DOMAINS','')))),
 
     /**
      * Tenancy bootstrappers are executed when tenancy is initialized.
