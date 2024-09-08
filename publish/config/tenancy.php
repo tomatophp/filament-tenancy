f6b815b8d391ba442241c4ede866c60f9c84615b
Fady Mondy <info@3x1.io>
2024-09-08 14:24:23 +0300

    allow db seeder

--- publish/config/tenancy.php
+++ publish/config/tenancy.php
@@ -192,7 +192,7 @@ return [
      * Parameters used by the tenants:seed command.
      */
     'seeder_parameters' => [
-        '--class' => 'TomatoPHP\\FilamentTenancy\\database\\seeders\\TenantsSeeder', // root seeder class
+        // '--class' => '', // your seeder here
         // '--force' => true,
     ],
 ];
--- src/FilamentTenancyServiceProvider.php
+++ src/FilamentTenancyServiceProvider.php
@@ -34,7 +34,7 @@ class FilamentTenancyServiceProvider extends ServiceProvider
                 JobPipeline::make([
                     Jobs\CreateDatabase::class,
                     Jobs\MigrateDatabase::class,
-                    // Jobs\SeedDatabase::class,
+                    Jobs\SeedDatabase::class,
 
                     // Your own jobs to prepare the tenant.
                     // Provision API keys, create S3 buckets, anything you want!
